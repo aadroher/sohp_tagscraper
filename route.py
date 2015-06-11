43bfe3001513ce9e2421297865e8dff3dcdf7e65
Armand Adroher Salvia <aadroher@gmail.com>
2015-06-11 15:30:24 +0100

    First prototype

--- route.py
+++ route.py
@@ -9,8 +9,12 @@ so_url = "http://stackoverflow.com/"
 @app.route('/', methods=['GET'])
 def tag_stats():
     tag_scraper = TagScraper(url=so_url)
-    tag_stats = tag_scraper.get_tag_stats()
-    return render_template('tag_stats.html', tag_stats=tag_stats)
+    tags = tag_scraper.get_tags(tag_scraper.html_doc)
+    tag_freqs = tag_scraper.get_tag_freqs()
 
+    return render_template('tag_stats.html', tag_freqs=tag_freqs,
+                                            # tags=tags,
+                                            so_url=so_url,
+                                            req_time=tag_scraper.req_time)
 if __name__ == '__main__':
     app.run(debug=True)
--- tag_scraper.py
+++ tag_scraper.py
@@ -1,27 +1,34 @@
 from requests import get as http_get
 from bs4 import BeautifulSoup
 import re
+from datetime import datetime
 
 class TagScraper:
 
     def __init__(self, url=None):
         self.url = url
+        self.req_time = None
         self.load_html()
 
     def load_html(self):
         self.html_doc = BeautifulSoup(http_get(self.url).text)
+        self.req_time = datetime.now()
 
     def get_tags(self, html):
         rs = html.find_all('a', class_='post-tag')
         tags = [html_tag.string for html_tag in rs]
         return tags
 
-    def filter_tags(self, html):
-        
-
-    def get_tag_stats(self):
+    def get_tag_freqs(self):
+        patterns = {'php': '^php$',
+                    'python': '^python(-2\.7|-3\.x)?$',
+                    'java': '^java$'}
         tags = self.get_tags(self.html_doc)
-        # print(self.get_tags(self.html_doc))
-        return tags
+        freqs = {}
+        for k,pat in patterns.iteritems():
+            freqs[k] = self._tag_count(tags, pat)
+        return freqs
 
-
+    def _tag_count(self, tags, pat):
+         r = re.compile(pat, re.IGNORECASE)
+         return len(filter(r.match, tags))
